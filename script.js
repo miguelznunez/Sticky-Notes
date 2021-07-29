3945ddb090f556ddcd685ac3bcca89d738a991dc
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2021-07-29 16:28:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,47 @@
-const createBox = document.getElementsByClassName("createBox")[0];
-const notes = document.getElementsByClassName("notes")[0];
-const input = document.getElementById("user-input");
-var contentArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
-var i = 0;
+var random_margin = ["-5px", "1px", "5px", "10px", "7px"];
+var random_colors = ["#c2ff3d","#ff3de8","#3dc2ff","#04e022","#bc83e6","#ebb328"];
+var random_degree = ["rotate(3deg)", "rotate(1deg)", "rotate(-1deg)", "rotate(-3deg)", "rotate(-5deg)", "rotate(-8deg)"];
+var index = 0;
 
-const margin = () => {
-  var random_margin = ["-5px","1px", "5px", "10px","15px","20px"];
-  return random_margin[Math.floor(Math.random() * random_margin.length)];
-}
+window.onload = document.querySelector("#user_input").select();
 
-const rotate = () => {
-  var random_degree = ["rotate(3deg)","rotate(1deg)","rotate(-1deg)","rotate(-3deg)","rotate(-5deg)", "rotate(-10deg)"];
-  return random_degree[Math.floor(Math.random() * random_degree.length)];
-}
+document.querySelector("#add_note").addEventListener("click", () => {
+  document.querySelector("#modal").style.display = "block";
+});
 
-const color = () => {
-  var random_colors = ["#c2ff3d","#ff3de8","#3dc2ff","#04e022","#bc83e6","#ebb328"];
-  if(i > random_colors.length - 1){
-    i = 0;
-  }
-  return random_colors[i++];
-}
+document.querySelector("#hide").addEventListener("click", () => {
+  document.querySelector("#modal").style.display = "none";
+});
 
-createBox.addEventListener('keydown', (event) => {
+document.querySelector("#user_input").addEventListener('keydown', (event) => {
   if(event.key === 'Enter'){
-    contentArray.push(input.value);
-    localStorage.setItem('items', JSON.stringify(contentArray));
-    divMaker(input.value);
-    input.value = '';
+    const text = document.querySelector("#user_input");
+    createStickyNote(text.value);
   }
 });
 
-document.getElementById("createNote").addEventListener("click", () => {
-  createBox.style.display = "block";
-});
+createStickyNote = (text) => {
+  let note = document.createElement("div");
+  let details = document.createElement("div");
+  let noteText = document.createElement("h1");
 
-document.getElementById("hide").addEventListener("click", () =>{
-  createBox.style.display = "none";
-});
+  note.className = "note";
+  details.className = "details";
+  noteText.textContent = text;
 
-document.getElementById("deleteNotes").addEventListener("click", () => {
-  localStorage.clear();
-  notes.innerHTML = '';
-  contentArray = [];
-});
+  details.appendChild(noteText);
+  note.appendChild(details);
 
-contentArray.forEach(divMaker);
+  if(index > random_colors.length - 1)
+    index = 0;
 
-function divMaker(text){
-  var div = document.createElement("div");
-  div.className = 'note';
-  div.innerHTML = '<div class="details">'+
-                    '<h1>'+text+'</h1>'+
-                  '</div>';
+  note.setAttribute("style", `margin:${random_margin[Math.floor(Math.random() * random_margin.length)]}; background-color:${random_colors[index++]}; transform:${random_degree[Math.floor(Math.random() * random_degree.length)]}`);
 
-  div.setAttribute('style', 'margin:'+margin() +'; transform:'+rotate()+'; background:'+color()+'');
-
-  notes.appendChild(div);
-
-  div.addEventListener("mouseenter", () => {
-    div.style.transform = "scale(1.1)";
+  note.addEventListener("dblclick", () => {
+    note.remove();
   })
 
-  div.addEventListener("mouseleave", () => {
-    div.style.transform = "scale(1)";
-    div.style.transform = rotate();
-  })
+  document.querySelector("#all_notes").appendChild(note);
 }
+
+
